d5a012a1ecf3651a5b7c82a7e060f6923ac00f7f
kko0831 <kko0831@hanmail.net>
2020-03-06 22:19:23 +0900

    타입스크립트 코리아 : 2017.06 React-with-TypeScript 세미나 (7) - react-router 맛보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Home = () => {
+  return <h3>Home</h3>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/intro">소개</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route exact={true} path="/" component={Home} />
+          <Route path="/intro" render={() => <h3>소개</h3>} />
+        </header>
+      </div>
+    </Router>
   );
 }
 
