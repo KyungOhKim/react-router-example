fce77f04bdab8b608a7b05b12e60c8cc92d1259d
kko0831 <kko0831@hanmail.net>
2020-03-08 16:18:14 +0900

    타입스크립트 코리아 : 2017.06 React-with-TypeScript 세미나 (8) - react-router props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  RouteComponentProps
+} from "react-router-dom";
 
 const Home = () => {
   return <h3>Home</h3>;
+};
+
+const Post = (props: RouteComponentProps<{ postId: string }>) => {
+  function goNextPost() {
+    const nextPostId = +props.match.params.postId + 1;
+    props.history.push(`/posts/${nextPostId}`);
+  }
+
+  return (
+    <div>
+      <h3>Post {props.match.params.postId}</h3>
+      <button onClick={goNextPost}>Next Post</button>
+      <p>{new URLSearchParams(props.location.search).get("body")}</p>
+    </div>
+  );
 };
 
 function App() {
@@ -26,6 +46,7 @@ function App() {
           </nav>
           <Route exact={true} path="/" component={Home} />
           <Route path="/intro" render={() => <h3>소개</h3>} />
+          <Route path="/posts/:postId" component={Post} />
         </header>
       </div>
     </Router>
