3ec235b52b0bc405938fa51bbf6a50406dcb8bfb
kko0831 <kko0831@hanmail.net>
2020-03-11 15:37:40 +0900

    타입스크립트 코리아 : 2017.06 React-with-TypeScript 세미나 (9) - react-router etc

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import "./App.css";
 import {
   BrowserRouter as Router,
   Route,
-  Link,
-  RouteComponentProps
+  RouteComponentProps,
+  Switch,
+  Redirect,
+  NavLink
 } from "react-router-dom";
 
 const Home = () => {
@@ -26,6 +28,28 @@ const Post = (props: RouteComponentProps<{ postId: string }>) => {
   );
 };
 
+const PostList = (props: RouteComponentProps<{}>) => {
+  return (
+    <div>
+      <Route
+        exact={true}
+        path={`${props.match.url}`}
+        render={() => <h3>PostList</h3>}
+      />
+      <Route path={`${props.match.url}/:postId`} component={Post} />
+    </div>
+  );
+};
+
+const NotFound = () => {
+  return <h3>Not Found!!</h3>;
+};
+
+const Admin = () => {
+  const isAdmin = true;
+  return isAdmin ? <h3>Admin</h3> : <Redirect to="/" />;
+};
+
 function App() {
   return (
     <Router>
@@ -37,16 +61,30 @@ function App() {
           <nav>
             <ul>
               <li>
-                <Link to="/">Home</Link>
+                <NavLink exact={true} to="/" activeStyle={{ fontSize: 24 }}>
+                  Home
+                </NavLink>
               </li>
               <li>
-                <Link to="/intro">소개</Link>
+                <NavLink to="/intro" activeStyle={{ fontSize: 24 }}>
+                  소개
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/admin" activeStyle={{ fontSize: 24 }}>
+                  Admin
+                </NavLink>
               </li>
             </ul>
           </nav>
-          <Route exact={true} path="/" component={Home} />
-          <Route path="/intro" render={() => <h3>소개</h3>} />
-          <Route path="/posts/:postId" component={Post} />
+          <Switch>
+            <Route exact={true} path="/" component={Home} />
+            <Route path="/intro" render={() => <h3>소개</h3>} />
+            <Redirect from="/about" to="/intro" />
+            <Route path="/posts" component={PostList} />
+            <Route path="/admin" component={Admin} />
+            <Route component={NotFound} />
+          </Switch>
         </header>
       </div>
     </Router>
